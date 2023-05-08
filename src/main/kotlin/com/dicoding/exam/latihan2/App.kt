fa7aa8f04d20b5af906e56defcc9ceb8b23d188e
lexynotfound <raihanardila@gmail.com>
2023-05-08 23:24:34 +0700

    perbaikan saran dari review code dicoding

--- src/main/kotlin/com/dicoding/exam/latihan2/App.kt
+++ src/main/kotlin/com/dicoding/exam/latihan2/App.kt
@@ -22,8 +22,7 @@ package com.dicoding.exam.latihan2
  *  Jika valueC bernilai null, silakan tetapkan nilai 50 sebagai nilai default-nya
  */
 fun calculate(valueA: Int, valueB: Int, valueC: Int?): Int {
-    val c = valueC ?: 50
-    return valueA + (valueB - c)
+    return valueA + (valueB - (valueC ?: 50))
 }
 
 /**
